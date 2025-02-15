ffa90371c98bb3d5bac447661594f584851f6bdd
cherry329720 <213233373@seu.edu.cn>
2025-02-15 12:07:30 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    """生成斐波那契数列的前n项"""
+    a, b = 0, 1
+    sequence = []
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+print("斐波那契数列前10项:", fibonacci(10))
